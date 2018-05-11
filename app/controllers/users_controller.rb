37154295d6ea492369a468f9fb86eeee0fecdda4
SamChang045 <willpower03@gmail.com>
2018-05-12 07:34:01 +0800

    add show page of the public users, specially on the post items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  def show
+    @posts = @user.posts
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
