ad07d4329b479e9d7b3a5c2b4c0eb26944e5c52b
SamChang045 <willpower03@gmail.com>
2018-05-12 03:24:07 +0800

    get all the data in the feeds controller for other usage

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,2 +1,7 @@
 class FeedsController < ApplicationController
+    @user_count = User.all.count
+    @post_count = Post.all.count
+    @comment_count = Comment.all.count
+    @users = User.select(:id, :name, :comments_count).order(comments_count: :desc).limit(10)
+    @posts = Post.select(:id, :title, :comments_count, :updated_at).order(comments_count: :desc).limit(10)
 end
