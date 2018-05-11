032e2afb9d52416df4701dbf5286b7db7fe92db1
SamChang045 <willpower03@gmail.com>
2018-05-11 20:12:26 +0800

    add the update action for the admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,13 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.email == "admin@example.com"
+      flash[:alert] = "Admin has no right to change!"
+      redirect_to admin_users_path
+    end
+  end
 end
