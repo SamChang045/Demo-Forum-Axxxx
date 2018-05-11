a66dc7b29970001bf5662b4f58f6c8ce8fa2dc49
SamChang045 <willpower03@gmail.com>
2018-05-12 07:43:54 +0800

    add the warning message when upgrading or downgrading the user role

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,6 +8,14 @@ class Admin::UsersController < ApplicationController
     if @user.email == "admin@example.com"
       flash[:alert] = "Admin has no right to change!"
       redirect_to admin_users_path
+    else
+      @user.update(role: params[:role])
+      if params[:role] == 'admin'
+        flash[:notice] = "#{@user.name} is being upgraded to #{params[:role]}"
+      else
+        flash[:notice] = "#{@user.name} is being downgraded to #{params[:role]}"
+      end
+      redirect_to admin_users_path
     end
   end
 end
