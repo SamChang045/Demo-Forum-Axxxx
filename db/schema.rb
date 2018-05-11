dbad30b18ba43a7869a460166ae16e5fc4c7f3a1
SamChang045 <willpower03@gmail.com>
2018-05-11 22:28:50 +0800

    add the category_id to the posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180511113313) do
+ActiveRecord::Schema.define(version: 20180511142539) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180511113313) do
     t.integer "viewed_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
