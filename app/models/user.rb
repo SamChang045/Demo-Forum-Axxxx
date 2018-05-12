426fba6dea1796f75e1e81dfb5339fac3fb47af0
SamChang045 <willpower03@gmail.com>
2018-05-13 00:31:13 +0800

    add the related friendships link in the user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,10 +17,17 @@ class User < ApplicationRecord
   has_many :collections, dependent: :destroy
   has_many :collect_posts, through: :collections, source: :post
 
-  has_many :friendships, dependent: :destroy
-  has_many :friends, through: :friendships
-  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverse_friendships, -> {where status: true}, class_name: "Friendship", foreign_key: "friend_id"
   has_many :inverse_friends, through: :inverse_friendships, source: :user
+
+  has_many :friendships, -> {where status: true}, dependent: :destroy
+  has_many :friends, through: :friendships
+
+  has_many :not_yet_accepted_by_friendships, -> {where status: false}, class_name: "Friendship", dependent: :destroy
+  has_many :not_yet_accepted_by_friends, through: :not_yet_accepted_by_friendships, source: :friend
+
+  has_many :not_yet_responded_to_friendships, -> {where status: false}, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+  has_many :not_yet_responded_to_friends, through: :not_yet_responded_to_friendships, source: :user
 
   def admin?
     self.role == "admin"
@@ -28,6 +35,14 @@ class User < ApplicationRecord
 
   def friend?(user)
     self.friendships.include?(user)
+  end
+
+  def not_yet_accepted_by?(user)
+    self.not_yet_accepted_by_friends.include?(user)
+  end
+
+  def not_yet_responded_to?(user)
+    self.not_yet_responded_to_friends.include?(user)
   end
 
   def all_friends
