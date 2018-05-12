ff51e7a189c5def4cde7162db15cced731b2ea3d
SamChang045 <willpower03@gmail.com>
2018-05-12 19:14:47 +0800

    add the latest reply time into the post db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180512031123) do
+ActiveRecord::Schema.define(version: 20180512111301) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20180512031123) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.datetime "last_replied_at"
   end
 
   create_table "users", force: :cascade do |t|
