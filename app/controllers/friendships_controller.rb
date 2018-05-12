a8ba8e210c45687b908993e4757077531d462a5b
SamChang045 <willpower03@gmail.com>
2018-05-13 04:12:39 +0800

    add friend function action into the post friend page.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,35 @@
 class FriendshipsController < ApplicationController
+  def create
+    @friendship = current_user.not_yet_accepted_by_friendships.build(friend_id: params[:friend_id])
+    @friendship.save
+    @user = User.find(params[:friend_id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def accept
+    @friendship = current_user.not_yet_responded_to_friendships.find_by(user_id: params[:id])
+    @friendship.update(status: true)
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def ignore
+    @friendship = current_user.not_yet_responded_to_friendships.find_by(user_id: params[:id])
+    @friendship.destroy
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def destroy
+    @friendship = current_user.friendships.where(following_id: params[:id]).first
+    @followship.destroy_all
+    flash[:alert] = "Friendship destroyed"
+    redirect_to friends_user_path(current_user)
+  end
 end
