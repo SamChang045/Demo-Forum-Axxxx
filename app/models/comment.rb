b719f4d5908efad1325e1dd2a822290ab207b9db
SamChang045 <willpower03@gmail.com>
2018-05-12 19:17:51 +0800

    update the layout of the posts index page.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ApplicationRecord
   validates_presence_of :content
   belongs_to :user, counter_cache: true
   belongs_to :post, counter_cache: true
+
+  after_save :update_post_last_replied_at
+
+  def update_post_last_replied_at
+    self.post.update(last_replied_at: self.updated_at)
+  end
 end
