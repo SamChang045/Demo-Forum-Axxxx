f1094926d35b5eb74d137a437dec49ba69b2a6bd
SamChang045 <willpower03@gmail.com>
2018-05-12 23:21:00 +0800

    rename the routes of the followingships to the friendships.

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     end    
   end
   
-  resources :followships, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
 
   resources :feeds, only: :index
   
