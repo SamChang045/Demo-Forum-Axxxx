0b5928231bb89283faf711f131172c29c071df77
SamChang045 <willpower03@gmail.com>
2018-05-14 05:05:38 +0800

    add authenticate TOKEN in the API action.

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,4 +57,8 @@ class User < ApplicationRecord
     friends = self.friends + self.inverse_friends
     return friends
   end
+
+  def generate_authentication_token
+     self.authentication_token = Devise.friendly_token
+  end
 end
