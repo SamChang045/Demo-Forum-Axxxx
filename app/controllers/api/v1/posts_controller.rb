ce703de9bad143120104d99148f668c1e5fdecfd
SamChang045 <willpower03@gmail.com>
2018-05-14 03:59:17 +0800

    add index and show action for post api.

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,33 @@
 class Api::V1::PostsController < ApplicationController
+
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @posts = Post.all
+    render json: {
+      data: @posts
+    }
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    if !@post
+      render json: {
+        error: "ERROR",
+        message: "Can't find the post!"
+      }
+    else
+      if Post.readable_posts(current_user).include?(@post)
+        @post.vieweds.create(user: current_user) unless @post.viewed_by?(current_user)
+        @comments = @post.comments
+        render json: @post
+      else
+        render json: {
+          error: "ERROR",
+          message: "Sorry! you have no right to do that."
+        }
+      end
+    end
+  end
+
 end
